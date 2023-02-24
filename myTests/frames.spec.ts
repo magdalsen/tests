1eb94f864dfbf58e7bf6d5d41424883da4c55f45
Wojciech Wieczorek <wojciech.wieczorek@contracted.pmi.com>
2023-02-24 09:43:46 +0100

    improve-initial-repo

--- myTests/frames.spec.ts
+++ myTests/frames.spec.ts
@@ -1,22 +1,23 @@
 import test, { expect } from "@playwright/test";
 
-test("Interact with frames", async ({page}) => {
-    await page.goto("https://letcode.in/frame");
-    const allframes = page.frames(); //give all frames on page
-    console.log("No.of frames: " + allframes.length);
-    
-    const myFrame = page.frame("firstFr");
-    await fillForm('magda', 'sen');
-    async function fillForm(fname: string, lname: string) {
-        if(myFrame) {
-            await myFrame.fill("input[name='fname']", fname);
-            await myFrame.fill("input[name='lname']", lname);
-            expect(await myFrame.locator("p.has-text-info").textContent()).toContain(`You have entered ${fname} ${lname}`)
-        }
-    }
+test("Interact with frames", async ({ page }) => {
+  await page.goto("https://letcode.in/frame");
+  const allframes = page.frames(); //give all frames on page
+  console.log("No.of frames: " + allframes.length);
 
-    const frame = page.frameLocator("#firstFr");
-    const innerFrame = frame.frameLocator("iframe[src='innerFrame']");
-    await innerFrame.locator("input[name='email']").fill("mmm@mmm.com");
-    
-})
+  const myFrame = page.frame("firstFr");
+  await fillForm("magda", "sen");
+  async function fillForm(fname: string, lname: string) {
+    if (myFrame) {
+      await myFrame.fill("input[name='fname']", fname);
+      await myFrame.fill("input[name='lname']", lname);
+      expect(await myFrame.locator("p.has-text-info").textContent()).toContain(
+        `You have entered ${fname} ${lname}`
+      );
+    }
+  }
+
+  const frame = page.frameLocator("#firstFr");
+  const innerFrame = frame.frameLocator("iframe[src='innerFrame']");
+  await innerFrame.locator("input[name='email']").fill("mmm@mmm.com");
+});
