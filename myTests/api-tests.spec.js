00cb23ac4dedc36c998174ff8c0a85a39e9fe79f
Magdalena Senger <msenger@sii.pl>
2023-03-21 08:52:30 +0100

    api tests small fix

--- myTests/api-tests.spec.js
+++ myTests/api-tests.spec.js
@@ -4,18 +4,6 @@ export const ID = 10;
 export const NAME = "doggie";
 
 test.describe.parallel("API testing", () => {
-  test("Response status - GET method", async ({ request }) => {
-    const dogs = await request.get(`${process.env.BASE_URL_PETSTORE}/pet/${ID}`);
-    expect(dogs.status()).toBe(200);
-  });
-
-  test("Find pet by ID", async ({ request }) => {
-    const dogs = await request.get(`${process.env.BASE_URL_PETSTORE}/pet/${ID}`);
-    expect(await dogs.json()).toEqual(
-      expect.objectContaining(petShopResponses.get.petById)
-    );
-  });
-
   test("Add a new pet to the store", async ({ request }) => {
     const newPet = await request.post(`${process.env.BASE_URL_PETSTORE}/pet`, {
       data: petShopResponses.post.newPetDataRequest
@@ -26,6 +14,18 @@ test.describe.parallel("API testing", () => {
     expect(issues.ok()).toBeTruthy();
     expect(await issues.json()).toEqual(
       expect.objectContaining(petShopResponses.post.newPetDataResponse)
+    );
+  });
+
+  test("Response status - GET method", async ({ request }) => {
+    const dogs = await request.get(`${process.env.BASE_URL_PETSTORE}/pet/${ID}`);
+    expect(dogs.status()).toBe(200);
+  });
+
+  test("Find pet by ID", async ({ request }) => {
+    const dogs = await request.get(`${process.env.BASE_URL_PETSTORE}/pet/${ID}`);
+    expect(await dogs.json()).toEqual(
+      expect.objectContaining(petShopResponses.get.petById)
     );
   });
 });
