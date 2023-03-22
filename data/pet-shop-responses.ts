6ebee531b2f57f816e602281f75337ba33558d37
Magdalena Senger <msenger@sii.pl>
2023-03-22 13:26:49 +0100

    api tests improvements

--- data/pet-shop-responses.ts
+++ data/pet-shop-responses.ts
@@ -1,33 +1,66 @@
-const ID = 10;
-const NAME = "doggie";
-
 export const petShopResponses = {
   get: {
     petById: {
-      category: { id: 100, name: "Frankie" },
-      id: 100,
-      name: "Frankie",
+      category: { id: 10, name: "string" },
+      id: 10,
+      name: "doggie",
       photoUrls: ["string"],
-      status: "available",
-      tags: [{ id: 100, name: "Frankie" }]
+      status: "string",
+      tags: [{ id: 10, name: "string" }]
     }
   },
   post: {
     newPetDataRequest: {
-      id: 100,
+      category: { id: 0, name: "Jeremy" },
+      id: 11,
+      name: "dog",
+      photoUrls: [],
+      status: "available",
+      tags: [{ id: 0, name: "Nice" }]
+    }
+  },
+  put: {
+    petToUpdate: {
+      category: { id: 0, name: "Julia" },
+      id: 15,
+      name: "elepfant",
+      photoUrls: [],
+      status: "available",
+      tags: [{ id: 0, name: "Big" }]
+    },
+    newPetDataRequest: {
+      id: 15,
       category: {
-        id: 100,
-        name: "Frankie"
+        id: 15,
+        name: "JuliaUpd"
       },
-      name: "Frankie",
+      name: "JuliaUpd",
       photoUrls: ["string"],
       tags: [
         {
-          id: 100,
-          name: "Frankie"
+          id: 15,
+          name: "JuliaUpd"
         }
       ],
       status: "available"
+    },
+    updatedPet: {
+      category: { id: 15, name: "JuliaUpd" },
+      id: 15,
+      name: "JuliaUpd",
+      photoUrls: ["string"],
+      status: "available",
+      tags: [{ id: 15, name: "JuliaUpd" }]
+    }
+  },
+  delete: {
+    addToDelete: {
+      category: { id: 0, name: "Ax" },
+      id: 12,
+      name: "bird",
+      photoUrls: [],
+      status: "available",
+      tags: [{ id: 0, name: "Colorful" }]
     }
   }
 };
--- myTests/CRUD-pet-store.spec.js
+++ myTests/CRUD-pet-store.spec.js
@@ -1,7 +1,6 @@
 import test, { expect } from "@playwright/test";
 import { petShopResponses } from "../data/pet-shop-responses";
-export const ID = 100;
-export const NAME = "doggie";
+export const ID = 10;
 
 test.describe.parallel("API testing", () => {
   test("Add a new pet to the store", async ({ request }) => {
@@ -9,6 +8,22 @@ test.describe.parallel("API testing", () => {
       data: petShopResponses.post.newPetDataRequest
     });
     expect(newPet.ok()).toBeTruthy();
+    let addedPet = await newPet.text();
+    let petJson = JSON.parse(addedPet);
+
+    const dogs = await request.get(
+      `${process.env.BASE_URL_PETSTORE}/pet/${petJson.id}`
+    );
+    expect(await dogs.json()).toEqual(
+      expect.objectContaining({
+        id: 11,
+        category: { id: 0, name: "Jeremy" },
+        name: "dog",
+        photoUrls: [],
+        tags: [{ id: 0, name: "Nice" }],
+        status: "available"
+      })
+    );
   });
 
   test("Find pet by ID", async ({ request }) => {
@@ -20,14 +35,31 @@ test.describe.parallel("API testing", () => {
   });
 
   test("Update pet in the store", async ({ request }) => {
+    await request.post(`${process.env.BASE_URL_PETSTORE}/pet`, {
+      data: petShopResponses.put.petToUpdate
+    });
+
     const pet = await request.put(`${process.env.BASE_URL_PETSTORE}/pet`, {
-      data: petShopResponses.post.newPetDataRequest
+      data: petShopResponses.put.newPetDataRequest
     });
     expect(pet.ok()).toBeTruthy();
+    const petData = await pet.text();
+    const parsedPet = JSON.parse(petData);
+
+    const dogs = await request.get(
+      `${process.env.BASE_URL_PETSTORE}/pet/${parsedPet.id}`
+    );
+    expect(await dogs.json()).toEqual(
+      expect.objectContaining(petShopResponses.put.updatedPet)
+    );
   });
 
   test("DELETE pet", async ({ request }) => {
-    const dogs = await request.delete(`${process.env.BASE_URL_PETSTORE}/pet/${ID}`);
+    await request.post(`${process.env.BASE_URL_PETSTORE}/pet`, {
+      data: petShopResponses.delete.addToDelete
+    });
+
+    const dogs = await request.delete(`${process.env.BASE_URL_PETSTORE}/pet/12`);
     expect(dogs.status()).toBe(200);
   });
 });
